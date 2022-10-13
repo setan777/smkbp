57e7dbca5afe4136f3e4bd839839e1716ccbc803
setan777 <69381660+setan777@users.noreply.github.com>
2022-10-13 11:41:59 +0700

    Add files via upload

---
+++ build/precache-manifest.6b527eaf10ebc32cc3ec5cacaf2cb83b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b01071016734f73e8a64553f6a203c",
+    "url": "/smkbp/index.html"
+  },
+  {
+    "revision": "a5a5b094b313ef381a50",
+    "url": "/smkbp/static/css/main.beaca61c.chunk.css"
+  },
+  {
+    "revision": "b6f6d58f0bde6aee5e66",
+    "url": "/smkbp/static/js/2.7a0967aa.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/smkbp/static/js/2.7a0967aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a5b094b313ef381a50",
+    "url": "/smkbp/static/js/main.2edec5b1.chunk.js"
+  },
+  {
+    "revision": "57ed377441f67b250e17",
+    "url": "/smkbp/static/js/runtime-main.d1eee971.js"
+  },
+  {
+    "revision": "0ad866d6cc64f385e662c64191f9ddaa",
+    "url": "/smkbp/static/media/IllustratorBiodata.0ad866d6.svg"
+  },
+  {
+    "revision": "5b61a38a4f77f4a5f31746e18a542812",
+    "url": "/smkbp/static/media/IllustratorFront.5b61a38a.png"
+  },
+  {
+    "revision": "140912a70eca4058c381d5a1162686bc",
+    "url": "/smkbp/static/media/IllustratorRegister.140912a7.svg"
+  },
+  {
+    "revision": "59e9b73d739080c27775093d2a816cb3",
+    "url": "/smkbp/static/media/IlustratorSeleksi.59e9b73d.svg"
+  },
+  {
+    "revision": "ceb36eac1575e41eea43827a1fb73552",
+    "url": "/smkbp/static/media/header.ceb36eac.svg"
+  },
+  {
+    "revision": "a1b213370d82aad35f73053032bcf63f",
+    "url": "/smkbp/static/media/hero_illustrator.a1b21337.svg"
+  },
+  {
+    "revision": "6ba9b87f931096711ebd6ea273241722",
+    "url": "/smkbp/static/media/png-elementary-school-primary-education-high-school-school-class-logo-high-school-elementary-school-clipart.6ba9b87f.png"
+  },
+  {
+    "revision": "d5b4f14dcbc67ec17e0691dab62f18ef",
+    "url": "/smkbp/static/media/user_group_two_color.d5b4f14d.svg"
+  }
+]);
